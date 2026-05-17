17d2274864666f181757ea29abd207c0112dab09
rubiyadav18 <rubiyadav20@navgurukul.org>
2026-05-17 14:49:20 +0530

    Refactor database connection code by removing commented-out legacy code and environment variable definitions

--- config/database.js
+++ config/database.js
@@ -1,20 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const connectDB = async () => {
-//   try {
-//     const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://rubiy564_db_user:< yr9yQo5aDXrKhFw9>@cluster0.xq2hm6j.mongodb.net/?appName=Cluster0', {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-
-//     console.log(`MongoDB Connected: ${conn.connection.host}`);
-//   } catch (error) {
-//     console.error('Error connecting to MongoDB:', error.message);
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = connectDB;
 
 
 const mongoose = require('mongoose');
@@ -36,9 +19,3 @@ const connectDB = async () => {
 module.exports = connectDB;
 
 
-
-
-// PORT=3000
-// MONGO_URI=mongodb+srv:rubiy564_db_user:yr9yQo5aDXrKhFw9@cluster0.xq2hm6j.mongodb.net/myDB?retryWrites=true&w=majority
-// JWT_SECRET=mysecretkey
-// NODE_ENV=production
